44d9aa9d41ce5d76bd8658da629067527226919a
bharti-12378 <bharti.ghildiyal@ankercloud.com>
2025-02-17 11:17:01 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,11 +1,10 @@
-terraform{
-required_providers {
-
+terraform {
+  required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = ">= 4.5.0"
     }
-}
+  }
 
   required_version = "~> 1.2"
 }
